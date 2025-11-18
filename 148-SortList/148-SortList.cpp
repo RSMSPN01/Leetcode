8157784ca49365e64c80f3789abc8de5f1238570
Praveen <praveennikhilbrothers@gmail.com>
2025-11-18 15:05:46 +0530

    Create 148-SortList

---
+++ 148-SortList/148-SortList.cpp
@@ -0,0 +1,33 @@
+// Last updated: 11/18/2025, 3:05:46 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ // 1st approach the naive way just store the values in the vector, sort it and then add the values back to it
+ // TC = O(nlogn + n+ n) and SC = O(n)
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode * temp = head;
+        vector<int>v;
+        while(temp!=nullptr){
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+        sort(v.begin(),v.end());
+        ListNode *temp2 = head;
+        int i = 0;
+        while(temp2!=nullptr){
+            temp2->val = v[i];
+            i++;
+            temp2 = temp2->next;
+        }
+        return head;
+    }
+};
