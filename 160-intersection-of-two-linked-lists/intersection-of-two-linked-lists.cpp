b9696df9d0b32c110cf2a6ffe0b69198d22c289f
Praveen <praveennikhilbrothers@gmail.com>
2025-11-18 18:26:33 +0530

    Time: 55 ms (11.87%) | Memory: 23.9 MB (5.02%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,10 +8,32 @@
  */
  // The naive approach is to store the node address and check if it is already there in the table
  // or not if not then insert it if already there then return the val respective to the node.
- // Efficient approach.
+ // TC = O(n+m) SC = O(n).
+ // Efficient approach. - not working because we can't modify the values of the string
+ // The same we use to check in inplace checking in array if we have already visited the index
+ // either we store it or we marked the values as -1 if we have already visited that.
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // ListNode*temp1 = headA;
+        // ListNode*temp2 = headB;
+
+        // This is efficient but we can't modify the values so yeah not working.... ):
+        // while(temp1!=nullptr){
+        //     // mark the values as -1 because we have visited them once.
+        //     temp1->val = -1;
+        //     temp1 = temp1->next;
+        // }
+        // while(temp2!=nullptr){
+        //     if(temp2->val <0){
+        //         return temp2;
+        //     }
+        //     temp2 = temp2->next;
+        // }
+        // return nullptr;
+
+
+        //Navie approach using extra space.
         unordered_set<ListNode*>s;
         ListNode*temp1 = headA;
         ListNode*temp2 = headB;
