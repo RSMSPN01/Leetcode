2a5f3937e85b602cca69c40e7eef09846bff7a86
Praveen <praveennikhilbrothers@gmail.com>
2025-12-11 20:13:49 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.cpp
@@ -0,0 +1,9 @@
+// Last updated: 12/11/2025, 8:13:49 PM
+class Solution {
+public:
+// Direct approach would we simple using inbuilt function sort 
+// but the time complexity is O(nlogn)
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    }
+};
