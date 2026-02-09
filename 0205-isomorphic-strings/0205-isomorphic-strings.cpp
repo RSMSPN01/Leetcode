c11dfecc4044e9da0877b6ff58c6b3969a048939
Praveen <praveenkumar.rsmspn@gmail.com>
2026-02-09 21:16:45 +0530

    Time: 2 ms (47.78%), Space: 9.2 MB (80.82%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -12,41 +12,65 @@
 // This code fails becuase of the fact that there are special character as well
 // and if i do '0' subtraction from the stirng it might give me values in
 // negative , so to avoid it typecast it directly
-// s[i] - '0' will give out of bound result 
+// s[i] - '0' will give out of bound result
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        // checking from the first side s string side
-        // empty array to track values
-        char freq[256] = {0};
-        for (int i = 0; i < s.length(); i++) {
-            // subtracting '0' will give result in index from 0
-            if (freq[(int)s[i]] == 0) {
-                // if it is empty store it
-                // freq[s[i] - '0'] = t[i];
-                freq[(int)s[i] ] = t[i];
 
-            } else {
-                // if not, check if pointing to same value or not
-                if (freq[(int)s[i]] != t[i]) {
-                    return false;
-                }
-            }
+        // Now this whole question can be solved in one traversal also
+        int n = s.length();
+        int m = t.length();
+        if (n != m) {
+            return false;
         }
-        // checking from the t string side
-        char freq2[256] = {0};
-        for (int i = 0; i < t.length(); i++) {
-            // subtracting 'a' will give result in index from 0
-            if (freq2[(int)t[i] ] == 0) {
-                // if it is empty store it
-                freq2[(int)t[i] ] = s[i];
+        char map1[256] = {0};
+        char map2[256] = {0};
+       
+        for (int i = 0; i < n; i++) {
+            if (map1[s[i]] == 0 && map2[t[i]] == 0) {
+                // it means both are not yet assigned
+                map1[s[i]] = t[i];
+                map2[t[i]] = s[i];
             } else {
-                // if not, check if pointing to same value or not
-                if (freq2[(int)t[i] ] != s[i]) {
+                // check if both points to same value or not
+                if (map1[s[i]] != t[i] || map2[t[i]] != s[i]) {
                     return false;
                 }
             }
         }
         return true;
+        // checking from the first side s string side
+        // empty array to track values
+
+        // char freq[256] = {0};
+        // for (int i = 0; i < s.length(); i++) {
+
+        //     if (freq[(int)s[i]] == 0) {
+        //         // if it is empty store it
+        //         // freq[s[i] - '0'] = t[i];
+        //         freq[(int)s[i] ] = t[i];
+
+        //     } else {
+        //         // if not, check if pointing to same value or not
+        //         if (freq[(int)s[i]] != t[i]) {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // // checking from the t string side
+        // char freq2[256] = {0};
+        // for (int i = 0; i < t.length(); i++) {
+
+        //     if (freq2[(int)t[i] ] == 0) {
+        //         // if it is empty store it
+        //         freq2[(int)t[i] ] = s[i];
+        //     } else {
+        //         // if not, check if pointing to same value or not
+        //         if (freq2[(int)t[i] ] != s[i]) {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
     }
 };
