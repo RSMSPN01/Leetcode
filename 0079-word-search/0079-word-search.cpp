0773aa549d890631dbbfcb061560b29139eafeb4
Praveen <praveenkumar.rsmspn@gmail.com>
2026-04-01 10:13:49 +0530

    Time: 914 ms (9.05%), Space: 11.5 MB (40.42%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,90 +1,43 @@
-// Brute Approach : I don't know how to solve this with backtracking but, a
-// general approach can be used in which we traverse the whole array and the
-// point where we find the first word we will start to find the word from there
+// This is the most optimal approach for this,earlier even if we find the answer
+// we still come back and do search for other paths but here if one path is
+// found directly return from there no need to search for other paths.
 
-// So the brute force approach is good only if we don't have duplicates, else it
-// will give error, when we multiple same letters going from one place
-
-// backtracking Approach : i have not solve any question like this so lets watch
-// video first and then comeback here. So the approach is very simple from each
-// index just look and keep on doing the backtrack
-
+// Athough the basic approach is already submitted earlier
 class Solution {
 public:
-    // Brute Force - Only works if there are no duplicates
-    // Time.C = O(n*m*k*(n+m)), Space.C = O(1)
-    // bool matchWord(vector<vector<char>>& board, string& word, int i, int j) {
-    //     // function help to find the word in O(m+n) time
-    //     int m = board.size();
-    //     int n = board[0].size();
-    //     for (int k = 1; k < word.length(); k++) {
-    //         bool top = false, bottom = false, right = false, left = false;
-    //         if (i != 0) {
-    //             top = true;
-    //         }
-    //         if (i != m - 1) {
-    //             bottom = true;
-    //         }
-    //         if (j != 0) {
-    //             left = true;
-    //         }
-    //         if (j != n - 1) {
-    //             right = true;
-    //         }
-    //         if (top && board[i - 1][j] == word[k]) {
-    //             i = i - 1;
-    //         } else if (bottom && board[i + 1][j] == word[k]) {
-    //             i = i + 1;
-    //         } else if (left && board[i][j - 1] == word[k]) {
-    //             j = j - 1;
-    //         } else if (right && board[i][j + 1] == word[k]) {
-    //             j = j + 1;
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-    // int m = board.size();
-    // int n = board[0].size();
-    // for (int i = 0; i < m; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         if (board[i][j] == word[0]) {
-    //             // search from the first word we found
-    //             if (matchWord(board, word, i, j)) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    // }
-    bool backtrack(vector<vector<char>>& board, string word, int i, int j,int ind) {
-        if (ind == word.length()) {
+    bool helper(vector<vector<char>>& board, string word, int row, int col,
+                int indx) {
+        // base case to handle all the values
+        if (indx == word.length()) {
             return true;
         }
-        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() ||
-                board[i][j] == '#' || board[i][j] != word[ind]) {
+        if (row < 0 || row >= board.size() || col < 0 ||
+            col >= board[0].size() || board[row][col] == '#') {
             return false;
         }
-        // simple backtrack template
-        char temp = board[i][j];
-        // include first
-        board[i][j] = '#';
-        // backtrack
-        bool result = backtrack(board, word, i - 1, j, ind + 1) ||
-                      backtrack(board, word, i + 1, j, ind + 1) ||
-                      backtrack(board, word, i, j - 1, ind + 1) ||
-                      backtrack(board, word, i, j + 1, ind + 1);
-        // exclude
-        board[i][j] = temp;
-        return result;
+        // check all the other four sides now
+        // if any time we reach end means return directly
+        if (board[row][col] == word[indx]) {
+            // mark visited
+            char temp = board[row][col];
+            board[row][col] = '#';
+            // explore ahead
+            if (helper(board, word, row + 1, col, indx + 1) ||
+                helper(board, word, row - 1, col, indx + 1) ||
+                helper(board, word, row, col + 1, indx + 1) ||
+                helper(board, word, row, col - 1, indx + 1)) {
+                return true;
+            }
+            // undo the changes
+            board[row][col] = temp;
+        }
+        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
                 if (board[i][j] == word[0]) {
-                    if (backtrack(board, word, i, j, 0)) {
+                    if (helper(board, word, i, j, 0)) {
                         return true;
                     }
                 }
